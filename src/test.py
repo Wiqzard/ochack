883fa2e32bb95c4c0c06b2cae9919639f4ea5d13
Wiqzard <s.stapf@aol.com>
2022-11-19 18:43:09 +0100

    docker added

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from torchvision.models.detection import (
-    fasterrcnn_resnet50_fpn_v2,
-    FasterRCNN_ResNet50_FPN_V2_Weights,
-)
-
-weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
-preprocess = weights.transforms()
-print(preprocess)
